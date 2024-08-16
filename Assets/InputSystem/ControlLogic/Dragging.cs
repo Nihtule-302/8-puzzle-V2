ffdc38c37d4021b6df63190cbac2674fda6c57b2
Ali wafa <30670268+Nihtule-302@users.noreply.github.com>
2024-08-17 02:07:49 +0300

    .

--- Assets/InputSystem/ControlLogic/Dragging.cs
+++ Assets/InputSystem/ControlLogic/Dragging.cs
@@ -2,45 +2,49 @@ using System;
 using nPuzzle.GridSystem;
 using UnityEngine;
 
-public class Dragging : MonoBehaviour, IControls
+namespace nPuzzle.InputSystem.ControlLogic
 {
-    [SerializeField] private GridSo gridInfo;
-    [SerializeField] private float movementThreshold = 0.5f;
-    private Vector3 _originalTilePosition;
-    private MouseInput _mouseInput;
-
-    private void Start()
+    public class Dragging : MonoBehaviour, IControls
     {
-        _mouseInput = GetComponent<MouseInput>();
-        _originalTilePosition = transform.position;
-    }
+        [SerializeField] private GridSo gridInfo;
+        [SerializeField] private float movementThreshold = 0.5f;
+        private Vector3 _originalTilePosition;
+        private MouseInput _mouseInput;
 
-    private void OnMouseDrag()
-    {
-        var currentMousePosition = _mouseInput.GetMouseWorldPosition();
-        var movementDirection = currentMousePosition - _originalTilePosition;
-
-        if (movementDirection.magnitude >= movementThreshold)
+        private void Start()
         {
-            var moveDirection = DetermineMoveDirection(movementDirection);
-            Move(moveDirection);
+            _mouseInput = GetComponent<MouseInput>();
+            _originalTilePosition = transform.position;
         }
-    }
 
-    private Vector2 DetermineMoveDirection(Vector3 movementDirection)
-    {
-        if (Math.Abs(movementDirection.x) >= Math.Abs(movementDirection.y))
+        private void OnMouseDrag()
         {
-            return movementDirection.x > 0 ? Vector2.right : Vector2.left;
-        }
-        else
-        {
-            return movementDirection.y > 0 ? Vector2.up : Vector2.down;
-        }
-    }
+            var currentMousePosition = _mouseInput.GetMouseWorldPosition();
+            var movementDirection = currentMousePosition - _originalTilePosition;
 
-    public void Move(Vector2 direction)
-    {
+            if (movementDirection.magnitude >= movementThreshold)
+            {
+                var moveDirection = DetermineMoveDirection(movementDirection);
+                Debug.Log(moveDirection.ToString());
+                Move(moveDirection);
+            }
+        }
+
+        private Vector2 DetermineMoveDirection(Vector3 movementDirection)
+        {
+            if (Math.Abs(movementDirection.x) >= Math.Abs(movementDirection.y))
+            {
+                return movementDirection.x > 0 ? Vector2.right : Vector2.left;
+            }
+            else
+            {
+                return movementDirection.y > 0 ? Vector2.up : Vector2.down;
+            }
+        }
+
+        public void Move(Vector2 direction)
+        {
         
+        }
     }
 }
